5e094675fcf4aec706db3611fd11b8d81a4515c0
Alina Pavalachi <pavalachi.alina@outlook.com>
2023-12-04 15:55:21 +0200

    adding hover effect

--- src/components/button/color-button.tsx
+++ src/components/button/color-button.tsx
@@ -1,4 +1,5 @@
 import './color-button.css';
+import {useState} from "react";
 export type btnProps =  {
   color: string;
   content: string;
@@ -6,11 +7,30 @@ export type btnProps =  {
   func: Function
   textColor: string;
   shadow: string;
+  activeColor: string;
 }
 export const ColorButton = (btn: btnProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const btnStyles = {
+    backgroundColor: isHovered ? btn.activeColor : btn.color,
+    width: btn.width,
+    color: btn.textColor,
+    borderBottom: `4px solid ${btn.shadow}`,
+  };
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div id='btn'
-         style={{backgroundColor: btn.color, width: btn.width, color: btn.textColor, borderBottom: `4px solid ${btn.shadow}`}}
+         onMouseEnter={ handleMouseEnter }
+         onMouseLeave={ handleMouseLeave }
+         style={btnStyles}
          onClick={() => {btn.func(btn.content)}}>
       {btn.content}
     </div>
--- src/components/calc-buttons/calc-buttons.tsx
+++ src/components/calc-buttons/calc-buttons.tsx
@@ -10,28 +10,28 @@ type CalcBtnsProps = {
 }
 export const CalcButtons = ({handleOperation, handleResult, handleNums, handleReset, handleDel}: CalcBtnsProps) => {
   const calcNumsArr = [
-    { content: '7', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '8', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '9', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#647198', content: 'DEL', func: handleDel, width: 101, textColor: '#fff', shadow: '#3A4663'},
+    { content: '7', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '8', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '9', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#647198', content: 'DEL', func: handleDel, width: 101, textColor: '#fff', shadow: '#3A4663', activeColor: '#A2B2E1'},
 
-    { content: '4', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '5', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '6', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '+', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797'},
+    { content: '4', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '5', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '6', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '+', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
 
-    { content: '1', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '2', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '3', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '-', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797'},
+    { content: '1', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '2', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '3', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '-', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
 
-    { content: '.', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '0', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: '/', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797'},
-    { color: '#EAE3DC', content: 'x', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797'},
+    { content: '.', color: '#EAE3DC', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '0', func: handleNums, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: '/', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
+    { color: '#EAE3DC', content: 'x', func: handleOperation, width: 101, textColor: '#434A59', shadow: '#979797', activeColor: '#fff'},
 
-    { content: 'RESET', color: '#647198', func: handleReset, width: 227, textColor: '#fff', shadow: '#3A4663'},
-    { color: '#D03F2F', content: '=', func: handleResult, width: 227, textColor: '#fff', shadow: '#94241c'},
+    { content: 'RESET', color: '#647198', func: handleReset, width: 227, textColor: '#fff', shadow: '#3A4663', activeColor: '#A2B2E1'},
+    { color: '#D03F2F', content: '=', func: handleResult, width: 227, textColor: '#fff', shadow: '#94241c', activeColor: '#F96B5B'},
 
 
 
@@ -47,6 +47,7 @@ export const CalcButtons = ({handleOperation, handleResult, handleNums, handleRe
             func={item.func}
             width={item.width}
             shadow={item.shadow}
+            activeColor={item.activeColor}
             textColor={item.textColor}></ColorButton>
         ))
       }
