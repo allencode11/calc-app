6c52cc2c9ea5666916b8333582fc12d3fcab5aec
Alina Pavalachi <pavalachi.alina@outlook.com>
2023-12-12 02:58:00 +0200

    modify for mobile version

--- src/components/buttons/secondary-btn/secondary-button.tsx
+++ src/components/buttons/secondary-btn/secondary-button.tsx
@@ -1,5 +1,6 @@
 import './secondary-button.css';
 import '../button.css';
+import {useEffect, useState} from "react";
 
 export type btnProps =  {
   content: string;
@@ -7,9 +8,29 @@ export type btnProps =  {
   func: Function
 }
 export const SecondaryButton = (btn: btnProps) => {
-  const w = btn.width == null ? 101 : btn.width;
+  const [divWidth, setDivWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 550) {
+       setDivWidth(btn.width == null ? 60 : 133);
+      } else {
+        setDivWidth(btn.width == null ? 101 : btn.width);
+      }
+    };
+
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [divWidth]);
 
   return (
-    <div id='btn' className='secondary' style={{width: w}} onClick={() => {btn.func(btn.content)}}>{btn.content}</div>
+    <div id='btn' className='secondary' style={{width: divWidth}} onClick={() => {btn.func(btn.content)}}>{btn.content}</div>
   )
 }
